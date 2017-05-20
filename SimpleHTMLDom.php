19ed1f896921b4d62595683383d459e20a78773d
Alfario Eka Putra <alfairoekaputra@gmail.com>
2017-05-20 22:33:05 +0700

    remove $offset in file_get_content which can cause error "stream does not support seeking"

--- SimpleHTMLDom.php
+++ SimpleHTMLDom.php
@@ -53,7 +53,7 @@
  * All of the Defines for the classes below.
  * @author S.C. Chen <me578022@gmail.com>
  */
-namespace keltstr\simplehtmldom;
+namespace rio\simplehtmldom;
 define( 'HDOM_TYPE_ELEMENT', 1 );
 define( 'HDOM_TYPE_COMMENT', 2 );
 define( 'HDOM_TYPE_TEXT', 3 );
@@ -86,7 +86,7 @@ class SimpleHTMLDom extends \yii\helpers\Inflector
         // We DO force the tags to be terminated.
         $dom      = new simple_html_dom( null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText );
         // For sourceforge users: uncomment the next line and comment the retreive_url_contents line 2 lines down if it is not already done.
-        $contents = file_get_contents( $url, $use_include_path, $context, $offset );
+        $contents = file_get_contents( $url, $use_include_path, $context );
         // Paperg - use our own mechanism for getting the contents as we want to control the timeout.
         //$contents = retrieve_url_contents($url);
         if ( empty( $contents ) || strlen( $contents ) > MAX_FILE_SIZE ) {
